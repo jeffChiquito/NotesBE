ef89517001bdd470cb83119b8f848222a074a495
Jeffry Chiquito <136511575+jeffChiquito@users.noreply.github.com>
2024-03-26 20:17:49 -0600

    Aplicacion de una sola pagina
    agregando los cambios necesarios para que la aplicacion funcione con un solo proyecto corriendo

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const app = express()
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqBody'))
 app.use(bodyParser.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 let notes = [
     {
