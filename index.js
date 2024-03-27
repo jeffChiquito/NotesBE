1f0b0cc68e826eab30218993c083987eeaa2be6a
Jeffry Chiquito <136511575+jeffChiquito@users.noreply.github.com>
2024-03-26 19:52:43 -0600

    NotesBE first commit
    hello world!

---
+++ index.js
@@ -0,0 +1,89 @@
+const { request } = require('express')
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+ morgan.token('reqBody', (request, response) => {
+     return JSON.stringify(request.body)
+ })
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqBody'))
+app.use(bodyParser.json())
+app.use(cors())
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
+    }
+  ]
+  
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)    
+    if (note) {
+        response.json(note)    
+    }else{
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+    
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: Boolean(body.important) || false, 
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+    response.json(note)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
