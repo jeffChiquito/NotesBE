92e8adf0d7d690779d5ca8a9bbd8d069614c3551
Jeffry Chiquito <136511575+jeffChiquito@users.noreply.github.com>
2024-04-24 22:50:07 -0600

    adding database connection to this application

--- index.js
+++ index.js
@@ -1,112 +1,124 @@
-const { request, response } = require('express')
+require('dotenv').config ()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const Note = require('./models/note')
+
 
 const app = express()
 
-morgan.token('reqBody', (request, response) => {
-    return JSON.stringify(request.body)
-})
+ morgan.token('reqBody', (request, response) => {
+     return JSON.stringify(request.body)
+ })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqBody'))
 app.use(bodyParser.json())
 app.use(cors())
 app.use(express.static('dist'))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      important: true
     },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+    {
+      id: 2,
+      content: "Browser can execute only JavaScript",
+      important: false
     },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      important: true
     }
-]
-
-app.get('/api/persons', (request, response) => {
-    response.json(persons)
+  ]
+  
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
-app.get('/info', (request, response) => {
-    const count = persons.length
-    const time = new Date()
-    response.send(`<p>phonebook has info for ${count} people </br></br>
-    ${time.toDateString()}</p>`)
-    
-})
-
-app.get('/api/persons/:id', (request, response) => {
-    const personID = Number(request.params.id)
-    const person = persons.find( val => val.id === personID)
-
-    if(!person){
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+    .then(note => {
+      if(note){
+        response.json(note)
+      }else{
         response.status(404).end()
-    }else{
-        response.json(person)
-    }
+      }      
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const personId = Number(request.params.id)
-    persons = persons.filter(person => person.id !== personId)
-    response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
-    const newId = Math.floor(Math.random() *500) + 1;
-
-
-    if (!body) {
-        return response.status(400).json({
-            error: 'content is missing'
-        })
-    }
-
-    if (!body.name) {
-        return response.status(400).json({
-            error: "Name is required"
-        })
-    }
-
-    if (!body.number) {
-        return response.status(400).json({
-            error: "Number is required"
-        })
-    }
-
-    if(persons.some(val => val.name == body.name)){
-        return response.status(409).send('Name is already existing')
-    }
     
-    const person = {
-        name: body.name,
-        number: body.number,
-        id: newId
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
     }
 
-    persons = persons.concat(person)
 
-    response.json(persons)
+    const note = new Note({
+        content: body.content,
+        important: body.important || false, 
+    })
+
+    note.save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
     
 })
 
-const PORT = process.env.PORT || 3001
+app.put('/api/notes/:id', (request, response, next) => {
+    const{content, important} = request.body
+
+
+    Note.findByIdAndUpdate(request.params.id, {content, important}, { new: true, runValidators: true, context:'query'})
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+
+//middleware controller handling error 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// este debe ser el último middleware cargado
+app.use(errorHandler)
---
+++ mongo.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length>3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = 
+`mongodb+srv://jeffrychiquito:${password}@cluster0.hycd9cz.mongodb.net/noteApp?
+retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        minLength: 5,
+        required: true
+    },
+    important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+Note.find({}).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
+})
