9f1c4e9b780cb7050c5368be3c4eca0ebab130d6
Jeffry Chiquito <136511575+jeffChiquito@users.noreply.github.com>
2024-04-25 23:54:36 -0600

    adding new version with db connection
    commit

--- index.js
+++ index.js
@@ -3,104 +3,136 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const Note = require('./models/note')
-
+const Person = require('./models/phone')
 
 const app = express()
 
- morgan.token('reqBody', (request, response) => {
-     return JSON.stringify(request.body)
- })
+morgan.token('reqBody', (request, response) => {
+    return JSON.stringify(request.body)
+})
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqBody'))
 app.use(bodyParser.json())
 app.use(cors())
 app.use(express.static('dist'))
 
+// let persons = [
+//     { 
+//       "id": 1,
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": 2,
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": 3,
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": 4,
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
-  
-app.get('/api/notes', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
-})
-
-app.get('/api/notes/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-    .then(note => {
-      if(note){
-        response.json(note)
-      }else{
-        response.status(404).end()
-      }      
-    })
+//mongoDB working
+app.get('/api/persons', (request, response, next) => {
+    Person.find({})
+    .then(persons => {
+        response.json(persons)
+    }) 
     .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response, next) => {
-    Note.findByIdAndDelete(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
-})
-
-const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
-
-app.post('/api/notes', (request, response, next) => {
-    const body = request.body
+//mongoDB working
+app.get('/info', (request, response, next) => {
     
-    if (!body.content) {
+    Person.find({})
+    .then(persons => {
+        const time = new Date()
+        response.send(`<p>phonebook has info for ${persons.length} people </br></br>
+        ${time.toDateString()}</p>`)
+    })
+    .catch(error => next(error))
+    
+    
+})
+
+//mongoDB working
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
+        response.json(person)
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response, next) => {
+    const body = request.body
+    const newId = Math.floor(Math.random() *500) + 1;
+
+
+    if (!body) {
         return response.status(400).json({
-            error: 'content missing'
+            error: 'content is missing'
         })
     }
 
+    if (!body.name) {
+        return response.status(400).json({
+            error: "Name is required"
+        })
+    }
 
-    const note = new Note({
-        content: body.content,
-        important: body.important || false, 
+    if (!body.number) {
+        return response.status(400).json({
+            error: "Number is required"
+        })
+    }
+
+    // if(persons.some(val => val.name == body.name)){
+    //     return response.status(409).send('Name is already existing')
+    // }
+    
+    const person = new Person({
+        name: body.name,
+        number: body.number,
+        id: newId
     })
 
-    note.save()
-    .then(savedNote => {
-      response.json(savedNote)
+    person.save()
+    .then(savedPerson => {
+        response.json(savedPerson)
     })
-    .catch(error => next(error))
+    .catch(error => next(error))    
     
 })
 
-app.put('/api/notes/:id', (request, response, next) => {
-    const{content, important} = request.body
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
 
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
 
-    Note.findByIdAndUpdate(request.params.id, {content, important}, { new: true, runValidators: true, context:'query'})
-    .then(updatedNote => {
-      response.json(updatedNote)
-    })
-    .catch(error => next(error))
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updatedPerson => {
+        response.json(updatedPerson)
+      })
+      .catch(error => next(error))
 })
 
 const PORT = process.env.PORT
@@ -111,14 +143,14 @@ app.listen(PORT, () => {
 
 //middleware controller handling error 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } 
-
-  next(error)
-}
-
-// este debe ser el último middleware cargado
-app.use(errorHandler)
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+  }
+  
+  // este debe ser el último middleware cargado
+  app.use(errorHandler)
--- mongo.js
+++ mongo.js
@@ -1,33 +1,64 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length>3) {
-    console.log('give password as argument')
-    process.exit(1)
-}
 
 const password = process.argv[2]
 
 const url = 
-`mongodb+srv://jeffrychiquito:${password}@cluster0.hycd9cz.mongodb.net/noteApp?
-retryWrites=true&w=majority`
+`mongodb+srv://jeffrychiquito:${password}@cluster0.hycd9cz.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
-    content: {
+const phonebookSchema = new mongoose.Schema({
+    name: {
         type: String,
-        minLength: 5,
+        minLength: 3,
         required: true
     },
-    important: Boolean,
+    number: Number
 })
 
-const Note = mongoose.model('Note', noteSchema)
+const Phone = mongoose.model('Phone', phonebookSchema)
 
-Note.find({}).then(result => {
-    result.forEach(note => {
-        console.log(note)
+
+if (process.argv.length == 3) {
+    Phone.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
     })
-    mongoose.connection.close()
-})
+}else if(process.argv.length == 5){
+    const phone = new Phone({
+        name: process.argv[3],
+        number: process.argv[4],
+    })
+    
+    phone.save().then(result => {
+        console.log(`Added ${phone.name} number ${phone.number} to phonebook`)
+        mongoose.connection.close()
+    })
+
+
+
+    const name = process.argv.slice(2)
+
+   
+
+    console.log(`nombre: ${name[1]}  numero: ${name[2]}`)
+    process.exit()
+
+    // const phone = new Phone({
+    //     name: process.argv[3],
+    //     number: process.argv[4],
+    // })
+    
+    // phone.save().then(result => {
+    //     console.log(`Added ${phone.name} number ${phone.number} to phonebook`)
+    //     mongoose.connection.close()
+    // })
+}else{
+    console.log('parameters for action are required');
+    process.exit()
+}
+
